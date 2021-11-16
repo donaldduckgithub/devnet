d4bb4dfc296efb8c1dad779606532a49134191e0
Kettern <akettern@infonline.de>
2021-11-16 02:55:30 +0100

    parse yaml, xml, json to python

---
+++ parseyaml.py
@@ -0,0 +1,14 @@
+import json
+import yaml
+
+yaml_file = open("myfile.yaml","r")
+
+pythondata = yaml.safe_load(yaml_file)
+
+print(pythondata['expires_in'])
+
+print("The access token from YAML is: %s" % pythondata['access_token'])
+
+print("\n\n")
+
+print(json.dumps(pythondata))
