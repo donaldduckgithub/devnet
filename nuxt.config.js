967db1fd1b8b435b17a24dbe815d8fe67210b9d5
Kettern <akettern@infonline.de>
2022-01-05 23:17:36 +0100

    changed baseurl from localhost to herokuapp

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,40 @@
+const createSitemapRoutes = async () => {
+  const routes = []
+  const { $content } = require('@nuxt/content')
+  const posts = await $content('posts').fetch()
+
+  for (const post of posts) {
+    routes.push(`/${post.slug}`)
+  }
+
+  const totalPosts = posts.length
+  const lastPage = Math.ceil(totalPosts / process.env.PER_PAGE)
+
+  for (let i = lastPage; i > 1; i--) {
+    routes.push(`/pages/${i}`)
+  }
+
+  return routes
+}
+
+const siteUrl = process.env.BASE_URL || 'http://http://turmburg.herokuapp.com'
+
+
 export default {
+  target: 'static',
+
+  publicRuntimeConfig: {
+    baseUrl: siteUrl,
+    perPage: process.env.PER_PAGE || '5',
+  },
+
+  
   // Target: https://go.nuxtjs.dev/config-target
-  ssr: false,
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'turmburg',
+    title: 'myblog2',
     htmlAttrs: {
       lang: 'en'
     },
@@ -16,6 +46,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      
     ]
   },
 
@@ -54,11 +85,33 @@ export default {
     middleware: ['password-protect']
   },
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  content: {
+    markdown: {
+      prism: {
+        theme: 'prism-themes/themes/prism-material-oceanic.css', //cool code window usw.
+      },
+    },
+  },
+
+  sitemap: {
+    hostname: siteUrl,
+    gzip: true,
+    routes: createSitemapRoutes,
+  },
+
+  generate: {
+    async routes() {
+      return await createSitemapRoutes()
+    },
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+
+
+    
   }
+
+  
+  
 }
