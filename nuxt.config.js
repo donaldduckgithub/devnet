3fbfe61dc136a0da8d1b14d0def9b322ec6f6512
Kettern <akettern@infonline.de>
2022-01-05 23:22:46 +0100

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,40 +1,10 @@
-const createSitemapRoutes = async () => {
-  const routes = []
-  const { $content } = require('@nuxt/content')
-  const posts = await $content('posts').fetch()
-
-  for (const post of posts) {
-    routes.push(`/${post.slug}`)
-  }
-
-  const totalPosts = posts.length
-  const lastPage = Math.ceil(totalPosts / process.env.PER_PAGE)
-
-  for (let i = lastPage; i > 1; i--) {
-    routes.push(`/pages/${i}`)
-  }
-
-  return routes
-}
-
-const siteUrl = process.env.BASE_URL || 'http://http://turmburg.herokuapp.com'
-
-
 export default {
-  target: 'static',
-
-  publicRuntimeConfig: {
-    baseUrl: siteUrl,
-    perPage: process.env.PER_PAGE || '5',
-  },
-
-  
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'myblog2',
+    title: 'turmburg',
     htmlAttrs: {
       lang: 'en'
     },
@@ -46,7 +16,6 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-      
     ]
   },
 
@@ -63,7 +32,6 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/tailwindcss',
     'nuxt-password-protect'
   ],
@@ -85,33 +53,11 @@ export default {
     middleware: ['password-protect']
   },
 
-  content: {
-    markdown: {
-      prism: {
-        theme: 'prism-themes/themes/prism-material-oceanic.css', //cool code window usw.
-      },
-    },
-  },
-
-  sitemap: {
-    hostname: siteUrl,
-    gzip: true,
-    routes: createSitemapRoutes,
-  },
-
-  generate: {
-    async routes() {
-      return await createSitemapRoutes()
-    },
-  },
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-
-
-    
   }
-
-  
-  
 }
