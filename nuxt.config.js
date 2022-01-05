223ad118dcc49dd1a0593d7954ed7102acc3efbe
Kettern <akettern@infonline.de>
2022-01-06 00:03:22 +0100

    hi

--- nuxt.config.js
+++ nuxt.config.js
@@ -81,7 +81,7 @@ export default {
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
+  modules: ['@nuxt/content'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
