574e483991d979152b50e21310baaa5005da957a
Kettern <akettern@infonline.de>
2022-01-05 23:37:08 +0100

    hi

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,40 +1,10 @@
-const createSitemapRoutes = async () => {
-  const routes = []
-  const { $content } = require('@nuxt/content')
-  const posts = await $content('posts').fetch()
-
-  for (const post of posts) {
-    routes.push(`/${post.slug}`)
-  }
-
-  const totalPosts = posts.length
-  const lastPage = Math.ceil(totalPosts / process.env.PER_PAGE)
-
-  for (let i = lastPage; i > 1; i--) {
-    routes.push(`/pages/${i}`)
-  }
-
-  return routes
-}
-
-const siteUrl = process.env.BASE_URL || 'http://localhost:3000'
-
-
 export default {
-  target: 'static',
-
-  publicRuntimeConfig: {
-    baseUrl: siteUrl,
-    perPage: process.env.PER_PAGE || '5',
-  },
-
-  
   // Target: https://go.nuxtjs.dev/config-target
   ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'myblog2',
+    title: 'turmburg',
     htmlAttrs: {
       lang: 'en'
     },
@@ -46,7 +16,6 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-      
     ]
   },
 
@@ -63,42 +32,32 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    'nuxt-password-protect'
   ],
+
+  passwordProtect: {
+    enabled: true,
+    formPath: '/password',
+    password: 'nilswolf1',
+    tokenSeed: 101010,
+    queryString: '_pw',
+    cookieName: '_password',
+    cookie: {
+      prefix: '',
+      expires: 5
+    },
+    ignoredPaths: ['/public-page']
+  },
+  router: {
+    middleware: ['password-protect']
+  },
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: ['@nuxt/content',
-  
+  modules: [
   ],
-
-  content: {
-    markdown: {
-      prism: {
-        theme: 'prism-themes/themes/prism-material-oceanic.css', //cool code window usw.
-      },
-    },
-  },
-
-  sitemap: {
-    hostname: siteUrl,
-    gzip: true,
-    routes: createSitemapRoutes,
-  },
-
-  generate: {
-    async routes() {
-      return await createSitemapRoutes()
-    },
-  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-
-
-    
   }
-
-  
-  
 }
