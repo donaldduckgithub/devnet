a434129d9124430feb5d2b6e37d8210d128f8d2e
Kettern <akettern@infonline.de>
2022-01-06 01:55:02 +0100

    hi

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,6 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     '@nuxtjs/tailwindcss',
-    '@nuxtjs/tailwindcss',
     'nuxt-password-protect'
   ],
 
