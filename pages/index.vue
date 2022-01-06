2f96ac390c97a6403c19d44fece1d5a715c03418
Kettern <akettern@infonline.de>
2022-01-06 04:01:52 +0100

    hi

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,21 @@
     <div class="container mx-auto py-5">
       <div class="flex flex-wrap text-center text-white">
         <div class="w-full md:w-2/3 p-5 border-r border-grey">
-
+          <div v-for="post in posts" :key="post.title" class="py-2">
+            <AppPostCard :post="post" />
+          </div>
         </div>
-
+        <div class="w-full md:w-1/3 p-5 md:text-left">
+    
+              <AppSearchInput />
+    
+        </div>
          <div class="w-full md:w-2/3 p-5 border-r border-white">
       <div class="flex justify-between text-center text-white">
         <button class="flex space-x-4 text-gray-200">&larr; Newer</button>
-
+        <NuxtLink to="/pages/2">
+          <button class="flex space-x-4 float-right">Older &rarr;</button>
+        </NuxtLink>
       </div>
       </div>
       </div>
@@ -18,16 +26,3 @@
 </div>
 </template>
 
-<script>
-export default {
-  async asyncData({ $content }) {
-    const posts = await $content('posts')
-      .only(['title', 'description', 'createdAt', 'slug'])
-      .sortBy('createdAt', 'desc')
-      .limit(process.env.PER_PAGE)
-      .fetch()
-
-    return { posts }
-  },
-}
-</script>
