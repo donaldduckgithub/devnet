4a1c51497ae241fb8f7204e9dfb4a48d7234ce51
Kettern <akettern@infonline.de>
2022-01-31 15:40:10 +0000

    updated git link

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -27,7 +27,7 @@
       <a href="https://www.youtube.com/playlist?list=PLgY1BWWbCdZxQSKYQAjkg6diVUA95nn1R" class="ml-6 mt-1 hover:text-white">
         Playlist
       </a>
-     <a href="https://github.com/donaldduckgithub/devnet" class="ml-6 mt-1 mr-7 hover:text-white">
+     <a href="https://github.com/donaldduckgithub/turmburg" class="ml-6 mt-1 mr-7 hover:text-white">
         GitHub
       </a>
       <a href="/shifts" aria-label="About page" class="mt-1 mr-7 hover:text-white nav-link nuxt-link-exact-active nuxt-link-active" aria-current="page">
